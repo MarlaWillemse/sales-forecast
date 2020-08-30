ece218ed676fa23078ab5e686a707b9674aa755c
Marla <marlawillem94@gmail.com>
2020-08-30 19:06:18 +0200

    run.py file working

--- run.py
+++
@@ -1,5 +0,0 @@
-root = '/home/marla/Desktop/sales_forecast'
-from preprocessing.data_utils import *
-from preprocessing.data_prep1 import *
-from models.xgboost_tune import *
-from models.xgboost_preds import *
